2bf2bfcaaacc98777a3abb4e418b4f35084b267c
Oluwasegun Yemi <67805792+oluyemmy@users.noreply.github.com>
2026-01-28 19:22:32 +0000

    Create APP with Flask

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 
+# import all libraries
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/new')
+def new():
+    return "Hello World is Awesome"
+
+
+
+if (__name__ == "__main__"):
+    app.run(host="0.0.0.0")
