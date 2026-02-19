f6985dcb1a7c2930913a7da4a762ea72f42ebcfb
Oluwasegun Yemi <67805792+oluyemmy@users.noreply.github.com>
2026-02-19 20:14:18 +0000

    metrics added and home message

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 
 # import all libraries
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+@app.route('/')
+def hello():
+    return "Flask Project on AWS"
 
 @app.route('/new')
 def new():
